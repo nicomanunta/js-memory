574af365c49a5b49922807f575055ab74929eb85
nicomanunta <nic.manun@gmail.com>
2025-01-17 16:51:34 +0100

    complete project

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let score = 0;
 
-// Array con le immagini del tema
-const images = [
+// array immagini 
+// marvel
+const marvels = [
     'images/marvel/spiderman.png',
     'images/marvel/spiderman2.png',
     'images/marvel/blackpanther.png',
@@ -18,26 +19,83 @@ const images = [
     'images/marvel/vision.png',
     'images/marvel/wolverine.png', 
 ];
-const cards = [...images, ...images]; // Doppio array per le coppie
+const marvelCards = [...marvels, ...marvels]; 
 
-// Mischia le carte
-cards.sort(() => 0.5 - Math.random());
 
-// Crea dinamicamente la griglia di carte
-cards.forEach(image => {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.innerHTML = `
-    <div class="card-inner">
-      <div class="card-front"></div>
-      <div class="card-back">
-      <img style="width: 100%" src="${image}" alt="">
-      </div>
-    </div>`;
-  grid.appendChild(card);
+// crea dinamicamente la griglia di carte
+marvelButton.addEventListener('click', () => {
+  
+  // svuoto la griglia 
+  grid.innerHTML = '';
+  
+  // mischio le carte
+  marvelCards.sort(() => 0.5 - Math.random());
 
-  card.addEventListener('click', flipCard);
-});
+  score = 0;
+  scoreDisplay.textContent = score;
+
+  marvelCards.forEach(marvel => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <div class="card-inner ">
+        <div class="card-front" style="background-image: url('images/marvel/sfondo-marvel.jpg');"></div>
+        <div class="card-back" style="background-color: rgb(122, 3, 3);">
+        <img style="width: 100%" src="${marvel}" alt="">
+        </div>
+      </div>`;
+    grid.appendChild(card);
+  
+    card.addEventListener('click', flipCard);
+  });
+
+})
+
+// pokemon
+const pokemons = [
+  'images/pokemon/bulbasaur.png',
+  'images/pokemon/charmender.png',
+  'images/pokemon/eevee.png',
+  'images/pokemon/meowth.png',
+  'images/pokemon/mew.png',
+  'images/pokemon/pikachu.png',
+  'images/pokemon/psyduck.png',
+  'images/pokemon/snorlax.png',
+  'images/pokemon/squirtle.png',
+  'images/pokemon/zubat.png', 
+];
+const pokemonCards = [...pokemons, ...pokemons]; 
+
+
+// crea dinamicamente la griglia di carte
+pokemonButton.addEventListener('click', () => {
+
+  // svuoto la griglia 
+  grid.innerHTML = '';
+  
+  // mischio le carte
+  pokemonCards.sort(() => 0.5 - Math.random());
+
+
+  score = 0;
+  scoreDisplay.textContent = score;
+
+  pokemonCards.forEach(pokemon => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <div class="card-inner">
+        <div class="card-front" style="background-image: url('images/pokemon/sfondo-pokemon.jpg');"></div>
+        <div class="card-back" style="background-color: #FFD54F;">
+        <img style="width: 100%" src="${pokemon}" alt="">
+        </div>
+      </div>`;
+    grid.appendChild(card);
+
+    card.addEventListener('click', flipCard);
+  });
+
+})
 
 function flipCard() {
   if (lockBoard) return;
